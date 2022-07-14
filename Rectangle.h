8da43f7a1321b2cbcc5b9f4564bad266a4e07a2a
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2022-07-13 18:48:44 -0700

    Rectangle Declaration

--- Rectangle.h
+++ Rectangle.h
@@ -10,7 +10,7 @@ public:
     Rectangle(QPaintDevice* device, int id, int width, int length, QPen pen, QBrush brush, int x = 0, int y = 0);
     ~Rectangle() override;
 
-    void draw(QPaintDevice* device) override;
+    void draw(int x, int y) override;
     void move(int x, int y) override;
     double area() override;
     double perimeter() override;
