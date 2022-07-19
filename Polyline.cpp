db9fb8fd098ec2436a303be04e42cc66de9c64be
alinaxrs <109118247+alinaxrs@users.noreply.github.com>
2022-07-19 03:36:40 -0700

    Update Polyline.cpp

--- Polyline.cpp
+++ Polyline.cpp
@@ -1,8 +1,25 @@
 #include "Polyline.h"
 
 Polyline::Polyline(QPaintDevice* device = nullptr, int id = -1)
-  : Shape(device, id, ShapeType::Polyline) 
-{ }
+  : Shape(device, id, ShapeType::Polyline), pointCount{0}
+{ 
+  for (int k = 0; k < SIZE; k++)
+  {
+    points[k].setX(0);
+    points[k].setY(0);
+  }
+}
+
+Polyline::Polyline(QPaintDevice* device, int id, QPen pen, QBrush brush, QPointF* points, int pointCount)
+    : Shape{device, id, ShapeType::Polyline, pen, brush}
+{
+    this->pointCount = pointCount;
+    for (int j = 0; j < pointCount; j++)
+    {
+        this->points[j].setX(points[j].x());
+        this->points[j].setY(points[j].y());
+    }
+}
 
 Polyline::~Polyline() {}
 
@@ -17,8 +34,36 @@ void Polyline::draw(QPaintDevice* device)
     painter.end();
 }
 
-/*void Polyline::move(const int x, const int y)
+void Polyline::move(const int newX, const int newY)
 {
-    this->x = x;
-    this->y = y;
-} */
+    for (int k = 0; k < pointCount; k++)
+    {
+        points[k].setX(points[k].x() + newX);
+        points[k].setY(points[k].y() + newY);
+    }
+}
+
+double Polyline::perimeter()
+{
+  return 0;
+}
+
+double Polyline::area()
+{
+  return 0;
+}
+
+void setNewCoords(int pointNum, int newX, int newY)
+{
+   points[pointNum] = QPointF(newX, newY);
+}
+
+int Polyline::getX(int pointNum) const
+{
+  return points[pointNum].x();
+}
+
+int Polyline::getY(int pointNum) const
+{
+  return points[pointNum].y();
+}
