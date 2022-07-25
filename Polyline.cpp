aa9d771563a77668ad8e103e96f8b78ca33aace0
Venkat-777 <65480658+Venkat-777@users.noreply.github.com>
2022-07-24 23:09:10 -0700

    Update Polyline.cpp

--- Polyline.cpp
+++ Polyline.cpp
@@ -1,34 +1,89 @@
-#ifndef POLYLINE_H
-#define POLYLINE_H
+#include "Polyline.h"
 
-#include "shape.h"
-#include "vector.h"
-#include <QPoint>
+Polyline::Polyline(int id )
+  : Shape(id), pointCount { 0 }, points { } { }
 
-using namespace MC_Vec;
 
-class Polyline : public Shape
+Polyline::Polyline(int id, Vector<QPoint> points)
+    : Shape(id), pointCount { pointCount }
 {
-public:
-    Polyline(int id = -1);
-    Polyline(int id, Vector<QPoint> points);
-    ~Polyline() override;
+    QPoint point;
 
-    void draw(QPaintDevice* device, QPainter* painter) override;
-    void move(const int newX, const int newY) override;
-    double perimeter() override;
-    double area() override;
+    for (QPoint* ptr {points.begin()}; ptr != points.end(); ++ptr)
+    {
+        point.setX(ptr->x());
+        point.setY(ptr->y());
 
-    int getX(int pointNum) const;
-    int getY(int pointNum) const;
-    Vector<QPoint> getPointVector();
+        this->points.push_back(point);
+    }
+}
 
-    void setX(int value, int point);
-    void setY(int value, int point);
 
-private:
-    int pointCount;
-    Vector<QPoint> points;
-};
+Polyline::~Polyline() { }
 
-#endif
+
+void Polyline::draw(QPaintDevice* device, QPainter* painter)
+{
+    QPoint qPoints[pointCount];
+
+    for (int i {0}; i < pointCount; ++i)
+        qPoints[i] = points[i];
+
+    painter->begin(device);
+    painter->setPen(getPen());
+    painter->setBrush(getBrush());
+    painter->drawPolyline(qPoints, pointCount);
+    painter->end();
+}
+
+
+void Polyline::move(const int newX, const int newY)
+{
+    for (int k = 0; k < pointCount; k++)
+    {
+        points[k].setX(points[k].x() + newX);
+        points[k].setY(points[k].y() + newY);
+    }
+}
+
+
+double Polyline::perimeter()
+{
+  return -1;
+}
+
+
+double Polyline::area()
+{
+  return -1;
+}
+
+
+int Polyline::getX(int pointNum) const
+{
+  return points[pointNum].x();
+}
+
+int Polyline::getY(int pointNum) const
+{
+  return points[pointNum].y();
+}
+
+Vector<QPoint> Polyline::getPointVector()
+{
+    return points;
+}
+
+void Polyline::setX(int value, int point)
+{
+    QPoint* qPoint { &points[point] };
+
+    qPoint->setX(value);
+}
+
+void Polyline::setY(int value, int point)
+{
+    QPoint* qPoint { &points[point] };
+
+    qPoint->setY(value);
+}
