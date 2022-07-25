1eb3a5bbe3c783231ec36dcec558ededcfae0fc4
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2022-07-24 20:29:49 -0700

    Update Line.h

--- Line.h
+++ Line.h
@@ -2,36 +2,37 @@
 #define LINE_H
 
 #include "shape.h"
-#include <vector>
+#include "vector.h"
 #include <cmath>
+
+using namespace MC_Vec;
 
 class Line : public Shape
 {
 private:
-    QPointF point1;
-    QPointF point2;
-public:
+    QPoint point1;
+    QPoint point2;
 
-    Line(QPaintDevice* device = nullptr, int id = -1);
-    Line(QPaintDevice* device,int id,QPen pen, QBrush brush, int x1,int x2,int y1,int y2);
+public:
+    Line(int id = -1);
+    Line(int id, QPen pen, QBrush brush, int x1,int x2,int y1,int y2);
+    Line(int id, Vector<QPoint> points);
     ~Line();
-    void draw(QPaintDevice* device) override;
-    void move(const int translateX, const int translateY) override;
+
+    void draw(QPaintDevice* device, QPainter* painter) override;
+    void move(const int x, const int y) override;
     double perimeter() override;
     double area() override;
 
-    //Gets
+    // Gets
     double getX1();
     double getY1();
     double getX2();
     double getY2();
 
-    //Sets
-    void setX1(double x1);
-    void setY1(double y1);
-    void setX2(double x2);
-    void setY2(double y2);
+    // Sets
+    void setX(int value, int point);
+    void setY(int value, int point);
 };
-
 
 #endif
