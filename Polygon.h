5206d53c3f148ccd6ed1ba83fcfde846d73d963c
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2022-07-24 20:31:36 -0700

    Update Polygon.h

--- Polygon.h
+++ Polygon.h
@@ -2,33 +2,33 @@
 #define POLYGON_H
 
 #include "shape.h"
-#include <vector>
+#include "vector.h"
+#include <QPoint>
+
+using namespace MC_Vec;
 
 class Polygon : public Shape
 {
 private:
     int numOfPoints;
-    std::vector<QPointF> points;
+    Vector<QPoint> points;
 
 public:
-    Polygon(QPaintDevice* device = nullptr, int id = -1);
-    Polygon(QPaintDevice* device,int id,QPen pen, QBrush brush, QPointF* points, int numOfSides );
+    Polygon(int id = -1);
+    Polygon(int id, Vector<QPoint> points);
+    Polygon(int id, QPen pen, QBrush brush, Vector<QPoint> points, int numOfpoints);
     ~Polygon() override;
-    
-    void draw(QPaintDevice* device) override;
+
+    void draw(QPaintDevice* device, QPainter* painter) override;
     void move(const int translateX, const int translateY) override;
     double perimeter() override;
     double area() override;
 
-    void setX(int x,int coordNum);
-    void setY(int y,int coordNum);
-
-    void newCoords(int x, int y);
-
     int getX() const;
     int getY() const;
 
+    void setX(int x, int coordNum);
+    void setY(int y, int coordNum);
 };
-
 
 #endif
