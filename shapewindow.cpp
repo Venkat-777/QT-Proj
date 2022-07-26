c6a83ac971c5b3a51b1227ba593ec63a29267188
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2022-07-25 19:19:03 -0700

    Update shapewindow.cpp

--- shapewindow.cpp
+++ shapewindow.cpp
@@ -79,15 +79,11 @@ void ShapeWindow::displayRectangleForm()
                                                   &ShapeWindow::changeY,
                                                   &ShapeWindow::changeW,
                                                   &ShapeWindow::changeH };
-    QLabel*      labels[4];
-    QSpinBox*    spinBoxes[4];
-    QHBoxLayout* horLayouts[4];
+    QSpinBox* spinBoxes[4];
 
     for (int index {0}; index < 4; ++index)
     {
-        labels[index]     = new QLabel(labelsText[index], ui->shapeEditor);
-        spinBoxes[index]  = new QSpinBox(ui->shapeEditor);
-        horLayouts[index] = new QHBoxLayout(ui->shapeEditor);
+        spinBoxes[index] = new QSpinBox(ui->shapeEditor);
 
         // set to the width of the canvas
         spinBoxes[index]->setMaximum(1400);
@@ -95,10 +91,7 @@ void ShapeWindow::displayRectangleForm()
         // Needs to set the current value of the shape!!
         spinBoxes[index]->setValue(100);
 
-        horLayouts[index]->addWidget(labels[index]);
-        horLayouts[index]->addWidget(spinBoxes[index]);
-
-        ui->shapeForm->addRow(horLayouts[index]);
+        ui->shapeForm->addRow(labelsText[index], spinBoxes[index]);
 
         QObject::connect(spinBoxes[index], &QSpinBox::valueChanged,
                          this,             spinFunctions[index]);
@@ -116,15 +109,11 @@ void ShapeWindow::displayEllipseForm()
                                                  &ShapeWindow::changeY,
                                                  &ShapeWindow::changeW,
                                                  &ShapeWindow::changeH };
-    QLabel*      labels[4];
-    QSpinBox*    spinBoxes[4];
-    QHBoxLayout* horLayouts[4];
+    QSpinBox* spinBoxes[4];
 
     for (int index {0}; index < 4; ++index)
     {
-        labels[index]     = new QLabel(labelsText[index], ui->shapeEditor);
-        spinBoxes[index]  = new QSpinBox(ui->shapeEditor);
-        horLayouts[index] = new QHBoxLayout(ui->shapeEditor);
+        spinBoxes[index] = new QSpinBox(ui->shapeEditor);
 
         // Set to the width of the canvas
         spinBoxes[index]->setMaximum(1400);
@@ -132,10 +121,7 @@ void ShapeWindow::displayEllipseForm()
         // Needs to set the current value of the shape!!
         spinBoxes[index]->setValue(100);
 
-        horLayouts[index]->addWidget(labels[index]);
-        horLayouts[index]->addWidget(spinBoxes[index]);
-
-        ui->shapeForm->addRow(horLayouts[index]);
+        ui->shapeForm->addRow(labelsText[index], spinBoxes[index]);
 
         QObject::connect(spinBoxes[index], &QSpinBox::valueChanged,
                          this,             spinFunctions[index]);
@@ -154,15 +140,11 @@ void ShapeWindow::displayLineForm()
                                                  &ShapeWindow::changeY1,
                                                  &ShapeWindow::changeX2,
                                                  &ShapeWindow::changeY2 };
-    QLabel*      labels[4];
-    QSpinBox*    spinBoxes[4];
-    QHBoxLayout* horLayouts[4];
+    QSpinBox* spinBoxes[4];
 
     for (int index {0}; index < 4; ++index)
     {
-        labels[index]     = new QLabel(labelsText[index], ui->shapeEditor);
         spinBoxes[index]  = new QSpinBox(ui->shapeEditor);
-        horLayouts[index] = new QHBoxLayout(ui->shapeEditor);
 
         // set to the width of the canvas.
         spinBoxes[index]->setMaximum(1400);
@@ -170,10 +152,7 @@ void ShapeWindow::displayLineForm()
         // Needs to set the current value of the shape!!
         spinBoxes[index]->setValue(100);
 
-        horLayouts[index]->addWidget(labels[index]);
-        horLayouts[index]->addWidget(spinBoxes[index]);
-
-        ui->shapeForm->addRow(horLayouts[index]);
+        ui->shapeForm->addRow(labelsText[index], spinBoxes[index]);
 
         QObject::connect(spinBoxes[index], &QSpinBox::valueChanged,
                          this,             spinFunctions[index]);
@@ -186,25 +165,21 @@ void ShapeWindow::displayLineForm()
 
 void ShapeWindow::displayPolyForm()
 {
+    const QString labelsText[12] { "X1:", "Y1:", "X2:", "Y2:",
+                                   "X3:", "Y3:", "X4:", "Y4:",
+                                   "X5:", "Y5:", "X6:", "Y6:" };
+
     void (ShapeWindow::*spinFunctions[8])(int) { &ShapeWindow::changeX1, &ShapeWindow::changeY1,
                                                  &ShapeWindow::changeX2, &ShapeWindow::changeY2,
                                                  &ShapeWindow::changeX3, &ShapeWindow::changeY3,
                                                  &ShapeWindow::changeX4, &ShapeWindow::changeY4
                                                                                                 };
-
-    const QString labelsText[12] { "X1:", "Y1:", "X2:", "Y2:",
-                                   "X3:", "Y3:", "X4:", "Y4:",
-                                   "X5:", "Y5:", "X6:", "Y6:" };
-    QLabel*      labels[8];
-    QSpinBox*    spinBoxes[8];
-    QHBoxLayout* horLayouts[8];
+    QSpinBox* spinBoxes[8];
 
     // update to dynamically get the number of points!!
      for (int index {0}; index < 8; ++index)
      {
-         labels[index]     = new QLabel(labelsText[index], ui->shapeEditor);
          spinBoxes[index]  = new QSpinBox(ui->shapeEditor);
-         horLayouts[index] = new QHBoxLayout(ui->shapeEditor);
 
          // set to the width of the canvas.
          spinBoxes[index]->setMaximum(1400);
@@ -212,10 +187,7 @@ void ShapeWindow::displayPolyForm()
          // Needs to set the current value of the shape!!
          spinBoxes[index]->setValue(100);
 
-         horLayouts[index]->addWidget(labels[index]);
-         horLayouts[index]->addWidget(spinBoxes[index]);
-
-         ui->shapeForm->addRow(horLayouts[index]);
+         ui->shapeForm->addRow(labelsText[index], spinBoxes[index]);
 
          QObject::connect(spinBoxes[index], &QSpinBox::valueChanged,
                           this,             spinFunctions[index]);
@@ -234,41 +206,26 @@ void ShapeWindow::displayTextForm()
                                                  &ShapeWindow::changeY,
                                                  &ShapeWindow::changeW,
                                                  &ShapeWindow::changeH };
-    QLabel*      labels[11];
-    QComboBox*   combos[5];
-    QHBoxLayout* horLayouts[11];
-    QLineEdit*   lineEdit;
-    QSpinBox*    spinBoxes[5];
+    QComboBox* combos[5];
+    QSpinBox*  spinBoxes[5];
+    QLineEdit*  lineEdit { new QLineEdit((QString)"The Text String", ui->shapeEditor) };
 
+    ui->shapeForm->addRow((QString)"TextString:", lineEdit);
 
-    // ** TextString **
-    labels[0]     = new QLabel((QString)"TextString:", ui->shapeEditor);
-    lineEdit      = new QLineEdit((QString)"The Text String", ui->shapeEditor);
-    horLayouts[0] = new QHBoxLayout(ui->shapeEditor);
-
-    horLayouts[0]->addWidget(labels[0]);
-    horLayouts[0]->addWidget(lineEdit);
-
-    ui->shapeForm->addRow(horLayouts[0]);
-
-    // Add connection...
+    QObject::connect(lineEdit, &QLineEdit::editingFinished, this, &ShapeWindow::changeText);
 
 
     // ** Text Dimensions & position **
     for (int index {0}; index < 4; ++index)
     {
-        labels[index+1]      = new QLabel(labelsText[index], ui->shapeEditor);
-        horLayouts[index+1]  = new QHBoxLayout(ui->shapeEditor);
-        spinBoxes[index]     = new QSpinBox(ui->shapeEditor);
+        spinBoxes[index] = new QSpinBox(ui->shapeEditor);
 
+        // adjust values!!
         spinBoxes[index]->setMinimum(0);
         spinBoxes[index]->setMaximum(1000);
         spinBoxes[index]->setValue(100);
 
-        horLayouts[index+1]->addWidget(labels[index+1]);
-        horLayouts[index+1]->addWidget(spinBoxes[index]);
-
-        ui->shapeForm->addRow(horLayouts[index+1]);
+        ui->shapeForm->addRow(labelsText[index], spinBoxes[index]);
 
         QObject::connect(spinBoxes[index], &QSpinBox::valueChanged,
                          this,             spinFunctions[index]);
@@ -276,18 +233,13 @@ void ShapeWindow::displayTextForm()
     }
 
     // ** TextPointSize **
-    labels[5]     = new QLabel((QString)"TextPointSize:", ui->shapeEditor);
     spinBoxes[4]  = new QSpinBox(ui->shapeEditor);
-    horLayouts[5] = new QHBoxLayout(ui->shapeEditor);
 
     spinBoxes[4]->setMinimum(-1);
     spinBoxes[4]->setMaximum(50);
     spinBoxes[4]->setValue(0);
 
-    horLayouts[5]->addWidget(labels[5]);
-    horLayouts[5]->addWidget(spinBoxes[4]);
-
-    ui->shapeForm->addRow(horLayouts[5]);
+    ui->shapeForm->addRow((QString)"TextPointSize:", spinBoxes[4]);
 
     QObject::connect(spinBoxes[4], &QSpinBox::valueChanged,
                      this,         &ShapeWindow::changeTextPointSize);
@@ -295,9 +247,7 @@ void ShapeWindow::displayTextForm()
 
 
     // ** TextColor **
-    labels[6] = new QLabel((QString)"TextColor:", ui->shapeEditor);
     combos[0] = new QComboBox(ui->shapeEditor);
-    horLayouts[6] = new QHBoxLayout(ui->shapeEditor);
 
     combos[0]->addItem((QIcon)":/img/white.png",   (QString)"White");
     combos[0]->addItem((QIcon)":/img/black.png",   (QString)"Black");
@@ -309,19 +259,14 @@ void ShapeWindow::displayTextForm()
     combos[0]->addItem((QIcon)":/img/yellow.png",  (QString)"Yellow");
     combos[0]->addItem((QIcon)":/img/gray.png",    (QString)"Gray");
 
-    horLayouts[6]->addWidget(labels[6]);
-    horLayouts[6]->addWidget(combos[0]);
-
-    ui->shapeForm->addRow(horLayouts[6]);
+    ui->shapeForm->addRow((QString)"TextColor:", combos[0]);
 
     QObject::connect(combos[0], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeTextColor);
 
 
     // ** TextAllignment **
-    labels[7]     = new QLabel((QString)"TextAllignment:", ui->shapeEditor);
-    combos[1]     = new QComboBox(ui->shapeEditor);
-    horLayouts[7] = new QHBoxLayout(ui->shapeEditor);
+    combos[1] = new QComboBox(ui->shapeEditor);
 
     combos[1]->addItem((QString)"AlignLeft");
     combos[1]->addItem((QString)"AlignRight");
@@ -329,10 +274,7 @@ void ShapeWindow::displayTextForm()
     combos[1]->addItem((QString)"AlignBottom");
     combos[1]->addItem((QString)"AlignCenter");
 
-    horLayouts[7]->addWidget(labels[7]);
-    horLayouts[7]->addWidget(combos[1]);
-
-    ui->shapeForm->addRow(horLayouts[7]);
+    ui->shapeForm->addRow((QString)"TextAllignment:", combos[1]);
 
     QObject::connect(combos[1], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeTextAllignment);
@@ -340,56 +282,43 @@ void ShapeWindow::displayTextForm()
 
 
     // ** TextFontFamily **
-    labels[8]     = new QLabel((QString)"TextFontFamily:", ui->shapeEditor);
-    combos[2]     = new QComboBox(ui->shapeEditor);
-    horLayouts[8] = new QHBoxLayout(ui->shapeEditor);
+    combos[2] = new QComboBox(ui->shapeEditor);
 
     combos[2]->addItem((QString)"Comic Sans MS");
     combos[2]->addItem((QString)"Courier");
     combos[2]->addItem((QString)"Helvetica");
     combos[2]->addItem((QString)"Times");
 
-    horLayouts[8]->addWidget(labels[8]);
-    horLayouts[8]->addWidget(combos[2]);
+    ui->shapeForm->addRow((QString)"TextFontFamily:", combos[2]);
 
-    ui->shapeForm->addRow(horLayouts[8]);
 
     QObject::connect(combos[2], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeTextFontFamily);
 
 
     // ** TextFontStyle **
-    labels[9]     = new QLabel((QString)"TextFontStyle:", ui->shapeEditor);
-    combos[3]     = new QComboBox(ui->shapeEditor);
-    horLayouts[9] = new QHBoxLayout(ui->shapeEditor);
+
+    combos[3] = new QComboBox(ui->shapeEditor);
 
     combos[3]->addItem((QString)"Normal");
     combos[3]->addItem((QString)"Italic");
     combos[3]->addItem((QString)"Oblique");
 
-    horLayouts[9]->addWidget(labels[9]);
-    horLayouts[9]->addWidget(combos[3]);
-
-    ui->shapeForm->addRow(horLayouts[9]);
+    ui->shapeForm->addRow((QString)"TextFontFamily:", combos[3]);
 
     QObject::connect(combos[3], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeTextFontStyle);
 
 
     // ** TextFontWeight **
-    labels[10]     = new QLabel((QString)"TextFontWeight:", ui->shapeEditor);
-    combos[4]      = new QComboBox(ui->shapeEditor);
-    horLayouts[10] = new QHBoxLayout(ui->shapeEditor);
+    combos[4] = new QComboBox(ui->shapeEditor);
 
     combos[4]->addItem((QString)"Thin");
     combos[4]->addItem((QString)"Light");
     combos[4]->addItem((QString)"Normal");
     combos[4]->addItem((QString)"Bold");
 
-    horLayouts[10]->addWidget(labels[10]);
-    horLayouts[10]->addWidget(combos[4]);
-
-    ui->shapeForm->addRow(horLayouts[10]);
+    ui->shapeForm->addRow((QString)"TextFontWeight:", combos[4]);
 
     QObject::connect(combos[4], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeTextFontWeight);
@@ -400,15 +329,10 @@ void ShapeWindow::displayTextForm()
 
 void ShapeWindow::displayPenBrushSettings()
 {
-    QLabel*      labels[6];
     QComboBox*   combos[6];
-    QHBoxLayout* horLayouts[6];
-
 
     // **  Pen Color **
-    labels[0]     = new QLabel((QString)"PenColor:", ui->shapeEditor);
     combos[0]     = new QComboBox(ui->shapeEditor);
-    horLayouts[0] = new QHBoxLayout(ui->shapeEditor);
 
     combos[0]->addItem((QIcon)":/img/white.png",   (QString)"White"  );
     combos[0]->addItem((QIcon)":/img/black.png",   (QString)"Black"  );
@@ -420,10 +344,7 @@ void ShapeWindow::displayPenBrushSettings()
     combos[0]->addItem((QIcon)":/img/yellow.png",  (QString)"Yellow" );
     combos[0]->addItem((QIcon)":/img/gray.png",    (QString)"Gray"   );
 
-    horLayouts[0]->addWidget(labels[0]);
-    horLayouts[0]->addWidget(combos[0]);
-
-    ui->shapeForm->addRow(horLayouts[0]);
+    ui->shapeForm->addRow((QString)"PenColor:", combos[0]);
 
     QObject::connect(combos[0], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changePenColor);
@@ -431,9 +352,7 @@ void ShapeWindow::displayPenBrushSettings()
 
 
     // ** PenStyle
-    labels[1]     = new QLabel((QString)"PenStyle:", ui->shapeEditor);
-    combos[1]     = new QComboBox(ui->shapeEditor);
-    horLayouts[1] = new QHBoxLayout(ui->shapeEditor);
+    combos[1] = new QComboBox(ui->shapeEditor);
 
     combos[1]->addItem((QString)"NoPen");
     combos[1]->addItem((QString)"SolidLine");
@@ -442,10 +361,7 @@ void ShapeWindow::displayPenBrushSettings()
     combos[1]->addItem((QString)"DashDotLine");
     combos[1]->addItem((QString)"DashDotDotLine");
 
-    horLayouts[1]->addWidget(labels[1]);
-    horLayouts[1]->addWidget(combos[1]);
-
-    ui->shapeForm->addRow(horLayouts[1]);
+    ui->shapeForm->addRow((QString)"PenStyle:", combos[1]);
 
     QObject::connect(combos[1], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changePenStyle);
@@ -453,18 +369,13 @@ void ShapeWindow::displayPenBrushSettings()
 
 
     // ** PenCapStyle
-    labels[2]     = new QLabel((QString)"PenCapStyle:", ui->shapeEditor);
-    combos[2]     = new QComboBox(ui->shapeEditor);
-    horLayouts[2] = new QHBoxLayout(ui->shapeEditor);
+    combos[2] = new QComboBox(ui->shapeEditor);
 
     combos[2]->addItem((QString)"FlatCap");
     combos[2]->addItem((QString)"SquareCap");
     combos[2]->addItem((QString)"RoundCap");
 
-    horLayouts[2]->addWidget(labels[2]);
-    horLayouts[2]->addWidget(combos[2]);
-
-    ui->shapeForm->addRow(horLayouts[2]);
+    ui->shapeForm->addRow((QString)"PenCapStyle:", combos[2]);
 
     QObject::connect(combos[2], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changePenCapStyle);
@@ -472,18 +383,13 @@ void ShapeWindow::displayPenBrushSettings()
 
 
     // ** PenJoinStyle
-    labels[3]     = new QLabel((QString)"PenJoinStyle:", ui->shapeEditor);
-    combos[3]     = new QComboBox(ui->shapeEditor);
-    horLayouts[3] = new QHBoxLayout(ui->shapeEditor);
+    combos[3] = new QComboBox(ui->shapeEditor);
 
     combos[3]->addItem((QString)"MiterJoin");
     combos[3]->addItem((QString)"BevelJoin");
     combos[3]->addItem((QString)"RoundJoin");
 
-    horLayouts[3]->addWidget(labels[3]);
-    horLayouts[3]->addWidget(combos[3]);
-
-    ui->shapeForm->addRow(horLayouts[3]);
+    ui->shapeForm->addRow((QString)"PenJoinStyle:", combos[3]);
 
     QObject::connect(combos[3], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changePenJoinStyle);
@@ -491,9 +397,7 @@ void ShapeWindow::displayPenBrushSettings()
 
 
     // ** BrushColor
-    labels[4]     = new QLabel((QString)"BrushColor:", ui->shapeEditor);
     combos[4]     = new QComboBox(ui->shapeEditor);
-    horLayouts[4] = new QHBoxLayout(ui->shapeEditor);
 
     combos[4]->addItem((QIcon)":/img/white.png",   (QString)"White");
     combos[4]->addItem((QIcon)":/img/black.png",   (QString)"Black");
@@ -505,10 +409,7 @@ void ShapeWindow::displayPenBrushSettings()
     combos[4]->addItem((QIcon)":/img/yellow.png",  (QString)"Yellow");
     combos[4]->addItem((QIcon)":/img/gray.png",    (QString)"Gray");
 
-    horLayouts[4]->addWidget(labels[4]);
-    horLayouts[4]->addWidget(combos[4]);
-
-    ui->shapeForm->addRow(horLayouts[4]);
+    ui->shapeForm->addRow((QString)"BrushColor:", combos[4]);
 
     QObject::connect(combos[4], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeBrushColor);
@@ -516,23 +417,18 @@ void ShapeWindow::displayPenBrushSettings()
 
 
     // ** BrushStyle
-    labels[5]     = new QLabel((QString)"BrushStyle:", ui->shapeEditor);
-    combos[5]     = new QComboBox(ui->shapeEditor);
-    horLayouts[5] = new QHBoxLayout(ui->shapeEditor);
+    combos[5] = new QComboBox(ui->shapeEditor);
+
 
     combos[5]->addItem((QString)"SolidPattern");
     combos[5]->addItem((QString)"HorPattern");
     combos[5]->addItem((QString)"VerPattern");
     combos[5]->addItem((QString)"NoBrush");
 
-    horLayouts[5]->addWidget(labels[5]);
-    horLayouts[5]->addWidget(combos[5]);
-
-    ui->shapeForm->addRow(horLayouts[5]);
+    ui->shapeForm->addRow((QString)"BrushStyle:", combos[5]);
 
     QObject::connect(combos[5], &QComboBox::currentTextChanged,
                      this,      &ShapeWindow::changeBrushColor);
-
 }
 
 
